f30ed44280ab6637987ce4fb195479bffcbc7d41
André Schneider <andre.schneider@posteo.de>
2015-07-30 14:43:46 +0200

    Added emacs configuration directory

---
+++ .emacs.d/init.el
@@ -0,0 +1,31 @@
+(load "~/.emacs.d/better-defaults.el")
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
+;; Set Linum-Mode on
+(global-linum-mode t)
+ 
+;; Linum-Mode and add space after the number
+(setq linum-format "%d ")
+
+;; Monokai Color Theme
+;(load "~/.emacs.d/plugins/color-theme-molokai.el")
+;(color-theme-molokai)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
